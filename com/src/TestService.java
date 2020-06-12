524e68937b8148c8929d792f83304834213f2098
niuzhaorong <zhaorong.niu@egym.de>
2020-06-12 12:24:04 +0800

    A_1_1

--- com/src/TestService.java
+++ com/src/TestService.java
@@ -2,8 +2,9 @@
  * @author: Zhaorong Niu
  * @since: 2020/6/12
  **/
-public class Test {
+public class TestService {
     public static void main(){
         System.out.println("hello");
+        System.out.println("hello world!!");
     }
 }
