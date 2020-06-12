0d8afa87d760b116a9df9bbff610fb50d3181644
zhniu886 <62987497+zhniu886@users.noreply.github.com>
2020-06-12 12:24:48 +0800

    A_1_1 (#5)
    * A_1
    
    * A_1
    
    * A_1_1
    
    Co-authored-by: niuzhaorong <zhaorong.niu@egym.de>

---
+++ com/src/TestService.java
@@ -0,0 +1,10 @@
+/**
+ * @author: Zhaorong Niu
+ * @since: 2020/6/12
+ **/
+public class TestService {
+    public static void main(){
+        System.out.println("hello");
+        System.out.println("hello world!!");
+    }
+}
